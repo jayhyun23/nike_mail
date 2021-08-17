dcd8156fefd2c84b5523c3bd5b0375024ec36e50
jay <kokikiko@towinpeoples.com>
2021-08-17 19:56:21 +0900

    Initial commit

---
+++ src/test/java/nike/mail/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nike.mail.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
